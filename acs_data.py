bba7a43783fe2a8531d7c686eb6a213f7bc9bfc3
Stephania Tello Zamudio <stello@linux7.cs.uchicago.edu>
2023-02-18 12:42:44 -0600

    only for illinois

--- acs_data.py
+++ acs_data.py
@@ -12,17 +12,20 @@ def pull_acs_data():
 
     Input: None
 
-    Returns (pd.DataFrame): zip code level demographics
+    Returns (pd.DataFrame): zip code level demographics for Illinois
     """
 
     acs_var = censusdata.download('acs5/subject', 2019, censusdata.censusgeo(
         [('zip%20code%20tabulation%20area', '*')]),
         ['S0601_C01_047E', 'S1901_C01_013E',
          'S1501_C02_009E', 'S1501_C02_012E',
-         'S2301_C04_001E'])
+         'S2301_C04_001E', 'STATE', 'ZCTA'])
+    
+    acs_var = acs_var[acs_var.STATE == 17]
+    acs_var = acs_var.drop('STATE', axis=1)
     
     acs_var.columns = ['hh_median_income', 'hh_mean_income',
                         'perc_educ_highschool', 'perc_educ_bachelor',
-                        'unemployment_rate']
+                        'unemployment_rate', 'zip_code']
     
     return acs_var
