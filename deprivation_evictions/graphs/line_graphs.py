48320c41603b152c03be9882854a76ff6dabc11f
Andrew Dunn <andrewdunn@linux1.cs.uchicago.edu>
2023-02-28 11:31:21 -0600

    updating line graph code

--- deprivation_evictions/graphs/line_graphs.py
+++ deprivation_evictions/graphs/line_graphs.py
@@ -6,14 +6,32 @@ import pandas as pd
 from dash import Dash, dcc, html
 import plotly.express as px
 
+# had to do 
+# pip3 install statsmodels
+# pip3 install -U psutil
+
 df = make_dummy_data()
-df.sort_values('num_evictions')
 
-# need to calculate averages for most of the columns
+# is this necessary?
+df = df.sort_values('num_evictions')
 
-fig = px.line(df, x = 'num_evictions', y = 'disparity_index', title='Comparison of Deprivation Index to Evictions')
+# Need to include other factors in the hover-over, like zip code
+
+
+fig = px.scatter(df, x="num_evictions", 
+                    y="disparity_index", 
+                    trendline="ols", 
+                    title='Comparison of Deprivation Index to Evictions')
 fig.show()
 
+
+# get the regression equation
+results = px.get_trendline_results(fig)
+results = results.iloc[0]["px_fit_results"].summary()
+# print(results)
+
+# correlation coefficient
+df['num_evictions'].corr(df['disparity_index'])
 
 # app = Dash(__name__)
 
