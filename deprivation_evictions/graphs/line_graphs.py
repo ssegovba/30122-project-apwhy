f670d0f346393791feae69e1ce5930410a9e0d92
Andrew Dunn <andrewdunn@linux1.cs.uchicago.edu>
2023-02-28 17:19:46 -0600

    tweaking scatter plot

--- deprivation_evictions/graphs/line_graphs.py
+++ deprivation_evictions/graphs/line_graphs.py
@@ -1,7 +1,6 @@
-# app.py
 
 
-from make_dummy_viz_data import make_dummy_data
+#from make_dummy_viz_data import make_dummy_data
 import pandas as pd
 from dash import Dash, dcc, html
 import plotly.express as px
@@ -10,28 +9,33 @@ import plotly.express as px
 # pip3 install statsmodels
 # pip3 install -U psutil
 
-df = make_dummy_data()
+#df = make_dummy_data()
 
 # is this necessary?
-df = df.sort_values('num_evictions')
+#df = df.sort_values('num_evictions')
 
 # Need to include other factors in the hover-over, like zip code
 
+# x_var = num_evictions
+# y_var = disparity_index
 
-fig = px.scatter(df, x="num_evictions", 
-                    y="disparity_index", 
-                    trendline="ols", 
-                    title='Comparison of Deprivation Index to Evictions')
-fig.show()
+def make_scatter_plot(df, x_var, y_var):
+    fig = px.scatter(df, x=x_var, 
+                        y=y_var, 
+                        trendline="ols", 
+                        title='Comparison of Deprivation Index to Evictions')
+    fig.show()
 
 
-# get the regression equation
-results = px.get_trendline_results(fig)
-results = results.iloc[0]["px_fit_results"].summary()
-# print(results)
+    # get the regression equation
+    results = px.get_trendline_results(fig)
+    results = results.iloc[0]["px_fit_results"].summary()
+    # print(results)
 
-# correlation coefficient
-df['num_evictions'].corr(df['disparity_index'])
+    # correlation coefficient
+    df['num_evictions'].corr(df['disparity_index'])
+
+    return fig
 
 # app = Dash(__name__)
 
