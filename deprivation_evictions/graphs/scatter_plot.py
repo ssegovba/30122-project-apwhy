bd1caf5950a869cb16cf8ff0a518431ba1601d1a
Andrew Dunn <andrewjtdunn@gmail.com>
2023-03-19 21:20:07 -0500

    cleaning up scatter plot code after linting

--- deprivation_evictions/graphs/scatter_plot.py
+++ deprivation_evictions/graphs/scatter_plot.py
@@ -1,13 +1,7 @@
 # Creates a scatter plot of zipcodes using two variables
 # Written by Andrew Dunn
 
-import pandas as pd
-from dash import Dash, dcc, html
 import plotly.express as px
-
-# to do:
-# update graph axis titles
-# round numbers in the hover
 
 def make_scatter_plot(df, x_var_label):
     '''
@@ -21,11 +15,11 @@ def make_scatter_plot(df, x_var_label):
         fig (plotly figure): the scatter plot with OLS line
     '''
 
-    # Define the x variables for different interactive axis labels 
+    # Define the x variables for different interactive axis labels
     if x_var_label == 'Deprivation Index':
         x_var = 'g1_sum_scaled'
     elif x_var_label == 'Violent Crime':
-        x_var = 'violent_crime_norm' 
+        x_var = 'violent_crime_norm'
     elif x_var_label == 'Non-Violent Crime':
         x_var = 'non_offensive_crime_norm'
     elif x_var_label == 'Rent-to-Income Ratio':
@@ -33,18 +27,18 @@ def make_scatter_plot(df, x_var_label):
     elif x_var_label == 'Time to the Loop':
         x_var = 'time_to_CBD_norm'
     elif x_var_label == 'Distance to the Loop':
-        x_var = 'distance_to_CBD_norm'     
-        
+        x_var = 'distance_to_CBD_norm'
+    
     # Build the graph
-    fig = px.scatter(df, x = x_var, 
-                        y = 'eviction_filings_completed', 
+    fig = px.scatter(df, x = x_var,
+                        y = 'eviction_filings_completed',
                         labels={
                             x_var : x_var_label,
                             "eviction_filings_completed" : "Number of Evictions (2019)",
                         },
-                        trendline = "ols", 
+                        trendline = "ols",
                         # Can we add in 'Zip code =' to the hover name?
-                        hover_name = "zipcode", 
+                        hover_name = "zipcode",
                         hover_data = [x_var, 'eviction_filings_completed'],
                         trendline_color_override = 'rgb(25, 137, 125)'
                     ).update_traces(
